1368f1489a5b55b8da552682ebb743e8cf987fbe
zwlfromcn <zwlfromcn@gmail.com>
2021-04-11 00:47:18 +0800

    创建品牌实体类的数据访问接口（Dao接口）

---
+++ qingcheng_service_goods/src/main/java/com/qingcheng/dao/BrandMapper.java
@@ -0,0 +1,10 @@
+package com.qingcheng.dao;
+
+import com.qingcheng.pojo.goods.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BrandMapper extends Mapper<Brand> {
+
+    /*继承了MyBaits的通用Mapper，内部实现了常用的CRUD操作*/
+
+}
